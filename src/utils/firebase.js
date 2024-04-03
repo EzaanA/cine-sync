a2ceb7786140aca63fd8bf127fb603ac67147e94
Sumanyu301 <sumanyukodan301@gmail.com>
2024-04-03 17:03:13 +0530

    Update dependencies and environment variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,21 +7,24 @@ import {
 } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAbowZDGw2RR4RSA81yIa-hmgV8t5mEyuU",
-  authDomain: "cine-sync-c1d7a.firebaseapp.com",
-  projectId: "cine-sync-c1d7a",
-  storageBucket: "cine-sync-c1d7a.appspot.com",
-  messagingSenderId: "887254496438",
-  appId: "1:887254496438:web:6e3ff12a6e7e192da2a217",
-  measurementId: "G-XBNT672VJ2",
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID,
 };
 
+console.log(import.meta.env.VITE_APP_API_KEY);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
@@ -29,4 +32,9 @@ const analytics = getAnalytics(app);
 // Get the Auth instance for the initialized Firebase app
 const auth = getAuth(app);
 
-export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged};
+export {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+};
